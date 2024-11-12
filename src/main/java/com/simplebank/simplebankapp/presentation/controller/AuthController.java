2d0165b7fcdd58ef7d334b1af7b17fdd51d5f76c
Jhon <jfaswis22@gmail.com>
2024-11-12 17:11:18 +0100

    feat: adds AuthController with signup and signin endpoints

---
+++ src/main/java/com/simplebank/simplebankapp/presentation/controller/AuthController.java
@@ -0,0 +1,27 @@
+package com.simplebank.simplebankapp.presentation.controller;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/auth")
+public class AuthController {
+    private final AuthService authService;
+
+    @PostMapping(value = "/signup")
+    public ResponseEntity<AuthResponse> signUp(@RequestBody @Valid SignUpRequest signUpRequest) {
+        return new ResponseEntity<>(authService.signUp(signUpRequest), HttpStatus.CREATED);
+    }
+
+    @PostMapping(value = "/signin")
+    public ResponseEntity<AuthResponse> signIn(@RequestBody @Valid SignInRequest signInRequest) {
+        return new ResponseEntity<>(authService.signIn(signInRequest), HttpStatus.OK);
+    }
+}
